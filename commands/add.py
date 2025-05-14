3e3a13b4bc0afc20589b9cb49ab1f039270de794
FrecceNere <118018187+FrecceNere@users.noreply.github.com>
2025-05-14 19:56:49 +0200

    add program basics

---
+++ commands/add.py
@@ -0,0 +1,4 @@
+import json
+
+def task(content):
+    pass 
---
+++ main.py
@@ -0,0 +1,38 @@
+# command imports
+from commands import add
+
+def welcome():
+    print("Welcome to the command line Task Tracker!")
+    print("help - to see the list of commands")
+    print("")
+    input_command()
+
+def input_command():
+    while True:
+        command = input("task-cli.py >" + " ")
+        command_handler(command)
+
+def command_handler(command):
+    if command == "help":
+        print("")
+        print("Available commands:")
+        print("add - to add a new task")
+        print("")
+
+    if command == "add":
+        content = input("Enter task content: ")
+        add.task(content)
+
+    elif command == "update":
+        pass
+    elif command == "delete":
+        pass
+    elif command == "mark-in-progress":
+        pass
+    elif command == "mark-done":
+        pass
+    elif command == "list":
+        pass
+
+if __name__ == "__main__":
+    welcome()       
